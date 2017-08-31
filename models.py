9c83c48d09f03ac5c1160bbf7f7484afe3fa32aa
morrillo <gustavo.orrillo@gmail.com>
2017-08-31 15:22:36 -0300

    Added support for pg_trm

--- models.py
+++ models.py
@@ -52,8 +52,10 @@ class sale_order(models.Model):
 		#import pdb;pdb.set_trace()
 		self.ensure_one()
 		if self.searchbox:
-			sql = "select a.id,b.id,b.name from product_product a inner join product_template b on a.product_tmpl_id = b.id where upper(b.name) like '%" + \
-				self.searchbox.upper() + "%'"
+			#sql = "select a.id,b.id,b.name from product_product a inner join product_template b on a.product_tmpl_id = b.id where upper(b.name) like '%" + \
+			#	self.searchbox.upper() + "%'"
+			sql = "select a.id,b.id,b.name from product_product a inner join product_template b on a.product_tmpl_id = b.id where '"+ \
+				self.searchbox + "' % name"
 			self.env.cr.execute(sql)
 			for a_id, b_id, b_name in self.env.cr.fetchall():
 				vals = {
